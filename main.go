cd5277e079979f76f535f6426e3dc52f06cf0bb5
Phil Evans <pde_repair@hotmail.com>
2024-02-22 22:05:28 +0000

    Initial Commit

---
+++ main.go
@@ -0,0 +1,18 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	fmt.Printf("Go\n")
+// }
+
+package main
+
+import "fmt"
+
+func main() {
+	age := 65
+	fmt.Printf("Age: %q\n", age)
+}
