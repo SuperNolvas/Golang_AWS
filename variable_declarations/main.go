d511825b07d3db1e93b4448032b525bab7d73d88
Phil Evans <pde_repair@hotmail.com>
2024-02-22 22:42:22 +0000

    Add variable declarations

---
+++ variable_declarations/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var VarInt = 42
+	var varString = "Test"
+	fmt.Printf("Value Int : %v\n", VarInt)
+	fmt.Printf("Type Int : %T\n", VarInt)
+	fmt.Printf("Value String : %v\n", varString)
+	fmt.Printf("Type String : %T\n", varString)
+}
