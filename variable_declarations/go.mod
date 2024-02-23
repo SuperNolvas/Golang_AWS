ce52e434a0f1c3d0ce5f6d225107f1236ccfc9f9
Phil Evans <pde_repair@hotmail.com>
2024-02-23 23:06:52 +0000

    Fix gopls error with command >  go mod init github.com/phil/variable_declarations

---
+++ variable_declarations/go.mod
@@ -0,0 +1,3 @@
+module github.com/phil/variable_declarations
+
+go 1.22.0
--- variable_declarations/main.go
+++ variable_declarations/main.go
@@ -5,8 +5,14 @@ import "fmt"
 func main() {
 	var VarInt int = 42
 	var varString = "Test"
+	var varFloat string
+	varFloat = "3.14"
+	varBool := true
+
 	fmt.Printf("Value Int : %v\n", VarInt)
 	fmt.Printf("Type Int : %T\n", VarInt)
 	fmt.Printf("Value String : %v\n", varString)
 	fmt.Printf("Type String : %T\n", varString)
+	fmt.Printf("Value Float : %v\n", varFloat)
+
 }
