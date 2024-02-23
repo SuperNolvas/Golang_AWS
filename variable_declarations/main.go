329daf37de9b59783ac25fd64eaf85c4d4f14e33
Phil Evans <pde_repair@hotmail.com>
2024-02-23 23:12:12 +0000

    Fix redudant variable string declaration

--- variable_declarations/main.go
+++ variable_declarations/main.go
@@ -5,14 +5,18 @@ import "fmt"
 func main() {
 	var VarInt int = 42
 	var varString = "Test"
-	var varFloat string
-	varFloat = "3.14"
+	var varFloat = "3.14"
 	varBool := true
 
 	fmt.Printf("Value Int : %v\n", VarInt)
 	fmt.Printf("Type Int : %T\n", VarInt)
+
 	fmt.Printf("Value String : %v\n", varString)
 	fmt.Printf("Type String : %T\n", varString)
-	fmt.Printf("Value Float : %v\n", varFloat)
 
+	fmt.Printf("Value Float : %v\n", varFloat)
+	fmt.Printf("Type Float : %T\n", varFloat)
+
+	fmt.Printf("Value Bool : %v\n", varBool)
+	fmt.Printf("Type Bool : %T\n", varBool)
 }
