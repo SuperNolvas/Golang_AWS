f55e5f3a6bac7d996137a03da96708ed7b22ddb3
Phil Evans <pde_repair@hotmail.com>
2024-02-28 00:43:09 +0000

    Experiment looping over array

--- arrays/main.go
+++ arrays/main.go
@@ -11,17 +11,29 @@
 // 	fmt.Println("Who are you?: " + names[2])
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	names := [...]string{"hello", "how", "are"}
+// 	var namePointer [3]*string // this is a pointer to a string
+// 	namePointer[0] = &names[0]
+// 	namePointer[1] = &names[2] // this is a pointer to the 3rd element. Position 0 and 1 of the array have values set. POsition 2 is nil as it has not been set.
+
+// 	for i, aPointer := range namePointer { // range returns the index and the value of the array
+// 		fmt.Println(i, " : ", aPointer) // this will print the index and the value of the array
+// 	}
+// }
+
 package main
 
 import "fmt"
 
 func main() {
 	names := [...]string{"hello", "how", "are"}
-	var namePointer [3]*string // this is a pointer to a string
-	namePointer[0] = &names[0]
-	namePointer[1] = &names[2] // this is a pointer to the 3rd element. Position 0 and 1 of the array have values set. POsition 2 is nil as it has not been set.
-
-	for i, aPointer := range namePointer { // range returns the index and the value of the array
-		fmt.Println(i, " : ", aPointer) // this will print the index and the value of the array
+	l := len(names)
+	for i := 0; i < l; i++ {
+		fmt.Println(i, " : ", names[i])
 	}
 }
