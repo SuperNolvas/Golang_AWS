514b293c407c0c15cf610ed35ab55774488a9a50
Phil Evans <pde_repair@hotmail.com>
2024-03-06 23:24:23 +0000

    More struct practice

--- structs/main.go
+++ structs/main.go
@@ -8,8 +8,17 @@ type Supe struct { // Struct name capitalized to make it public
 }
 
 func main() {
-	var shazam supe        // create a new superhero
-	shazam.name = "Shazam" // set the name of the superhero
-	shazam.strength = 100  // set the strength of the superhero
-	fmt.Println(shazam)    // print the superhero
+	var shazam Supe              // create a new superhero
+	shazam.name = "Shazam"       // set the name of the superhero
+	shazam.strength = 100        // set the strength of the superhero
+	fmt.Println(shazam)          // print the superhero
+	fmt.Println(shazam.name)     // print the name of the superhero
+	fmt.Println(shazam.strength) // print the strength of the superhero
+
+	son := shazam          // create another superhero
+	son.name = "shazamson" // create a new superhero and set it to the value of shazam
+	son.strength = 1000    // set the strength of the superhero
+	fmt.Println(son)       // print the superhero
+	fmt.Println(son.name)  // print the name of the superhero
+	fmt.Println(shazam.strength)
 }
